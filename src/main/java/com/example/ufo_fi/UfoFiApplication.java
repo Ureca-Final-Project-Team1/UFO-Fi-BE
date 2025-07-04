9556d9b387f40203c74ab9110ada21b78eb32220
Minjae-An <mj3242@naver.com>
2025-07-04 17:17:50 +0900

    chore: [UF-48] 프로젝트 초기 세팅

---
+++ src/main/java/com/example/ufo_fi/UfoFiApplication.java
@@ -0,0 +1,13 @@
+package com.example.ufo_fi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UfoFiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UfoFiApplication.class, args);
+	}
+
+}
