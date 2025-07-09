267d300fb4174d8f77508f3c2b923dd8554d4d98
jeongjiho <wjdwlgh2000@naver.com>
2025-07-09 14:06:10 +0900

    UF-59 refactor: ROLE_NO_INFO 응답형식 리팩터링

--- src/main/java/com/example/ufo_fi/global/security/exception/SecurityResponseSetter.java
+++ src/main/java/com/example/ufo_fi/global/security/exception/SecurityResponseSetter.java
@@ -16,28 +16,41 @@ import org.springframework.stereotype.Component;
 @Component
 public class SecurityResponseSetter {
     private static final String CONTENT_TYPE = "application/json;charset=UTF-8";
-    private static final String CODE = "code";
+    private static final String STATUS_CODE = "statusCode";
     private static final String MESSAGE = "message";
     private static final String DATA = "data";
     private static final String IDENTITY = "security ";
 
     private final ObjectMapper objectMapper = new ObjectMapper();
 
+    /**
+     * Security 단에서는 ResponseEntity를 사용할 수 없기 때문에, response에 담아 전달해야한다.
+     * 이 메서드는 security에서 나올 수 있는 예외를 response에 담아 전달하는 놈이다.
+     */
     public void setResponse(HttpServletResponse response, ErrorCode errorCode) throws IOException {
-        response.setContentType(CONTENT_TYPE);
+        response.setContentType(CONTENT_TYPE);                  //헤더 세팅
         response.setStatus(errorCode.getStatus().value());
+
         Map<String, Object> body = new HashMap<>();
-        body.put(CODE, errorCode.getStatus().value());
+        body.put(STATUS_CODE, errorCode.getStatus().value());  //바디 세팅
         body.put(MESSAGE, IDENTITY + errorCode.getMessage());
-        objectMapper.writeValue(response.getWriter(), body);
+
+        objectMapper.writeValue(response.getWriter(), body);    //response에 body를 직렬화
     }
 
+    /**
+     * Security 단에서는 ResponseEntity를 사용할 수 없기 때문에, response에 담아 전달해야한다.
+     * 이 메서드는 security에서 응답 바디를 세팅해주기 위한 놈이다.
+     */
     public <T> void setResponse(HttpServletResponse response, T data) throws IOException {
-        response.setContentType(CONTENT_TYPE);
+        response.setContentType(CONTENT_TYPE);                  //헤더 세팅
         response.setStatus(HttpStatus.OK.value());
-        Map<String, Object> body = new HashMap<>();
-        body.put(CODE, HttpStatus.OK.value());
+
+        Map<String, Object> body = new HashMap<>();             //바디 세팅
+        body.put(STATUS_CODE, HttpStatus.OK.value());
+        body.put(MESSAGE, "나이스~나이스~");
         body.put(DATA, data);
-        objectMapper.writeValue(response.getWriter(), body);
+
+        objectMapper.writeValue(response.getWriter(), body);    //response에 body를 직렬화
     }
 }
