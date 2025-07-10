6c748c331c4dcb491fcade6627a165563d4ba94f
minkyeong <stl9955@naver.com>
2025-07-10 13:36:25 +0900

    UF-108 feat: TestFirebaseConfig 추가

--- src/test/java/com/example/ufo_fi/UfoFiApplicationTests.java
+++ src/test/java/com/example/ufo_fi/UfoFiApplicationTests.java
@@ -1,11 +1,14 @@
 package com.example.ufo_fi;
 
+import com.example.ufo_fi.config.TestFirebaseConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 
 @ActiveProfiles("test")
 @SpringBootTest
+@Import(TestFirebaseConfig.class)
 class UfoFiApplicationTests {
 
     @Test
