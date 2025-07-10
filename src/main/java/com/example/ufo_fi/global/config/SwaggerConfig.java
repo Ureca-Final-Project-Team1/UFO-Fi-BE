5a57184da5769da4e3939f32fdb27c9e0b91a4b2
minkyeong <stl9955@naver.com>
2025-07-11 02:12:52 +0900

    UF-120 feat: SwaggerConfig 생성

---
+++ src/main/java/com/example/ufo_fi/global/config/SwaggerConfig.java
@@ -0,0 +1,35 @@
+package com.example.ufo_fi.global.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
+import io.swagger.v3.oas.models.OpenAPI;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@OpenAPIDefinition(
+        info = @Info(
+                title = "UFO-FI API",
+                description = "UFO-FI API 명세서",
+                version = "v.1.0"
+        ),
+        servers = {
+                @Server(url = "http://localhost:8080", description = "로컬 개발")
+        }
+)
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(createApiInfo());
+    }
+
+    private io.swagger.v3.oas.models.info.Info createApiInfo() {
+        return new io.swagger.v3.oas.models.info.Info()
+                .title("UFO-FI API")
+                .description("UFO-FI API 명세서")
+                .version("1.0.0");
+    }
+}
