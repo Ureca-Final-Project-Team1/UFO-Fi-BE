9c9f0c1fa8d2d06f34e3c51cf38a6e3a4b94d74f
minkyeong <stl9955@naver.com>
2025-07-13 01:38:10 +0900

    fix: [UF-117] Entity 이름 변경에 따른 Repository 수정

--- src/main/java/com/example/ufo_fi/domain/notification/repository/NotificationRepository.java
+++ src/main/java/com/example/ufo_fi/domain/notification/repository/NotificationRepository.java
@@ -1,10 +1,10 @@
 package com.example.ufo_fi.domain.notification.repository;
 
-import com.example.ufo_fi.domain.notification.entity.Notification;
+import com.example.ufo_fi.domain.notification.entity.NotificationSettings;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface NotificationRepository extends JpaRepository<Notification, Long> {
-    Optional<Notification> findByUserId(Long userId);
+public interface NotificationRepository extends JpaRepository<NotificationSettings, Long> {
+    Optional<NotificationSettings> findByUserId(Long userId);
 }
