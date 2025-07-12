d58d8c0a8726a13c40a8889e07fae3a76025265b
minkyeong <stl9955@naver.com>
2025-07-13 03:15:41 +0900

    fix: [UF-146] Swagger 설명 추가

--- src/main/java/com/example/ufo_fi/domain/notification/controller/api/FcmTokenApiSpec.java
+++ src/main/java/com/example/ufo_fi/domain/notification/controller/api/FcmTokenApiSpec.java
@@ -1,4 +1,4 @@
-package com.example.ufo_fi.domain.notification.controller;
+package com.example.ufo_fi.domain.notification.controller.api;
 
 import com.example.ufo_fi.domain.notification.dto.request.FcmTokenSaveReq;
 import com.example.ufo_fi.domain.notification.dto.response.FcmTokenCommonRes;
@@ -9,13 +9,15 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
-@Tag(name = "Fcm API", description = "FCM 토큰 관련 API")
-public interface FcmControllerApiSpec {
+@Tag(name = "FCM token API", description = "FCM 토큰 API")
+public interface FcmTokenApiSpec {
 
-    // TODO: 추후 인증 연동 시 @AuthenticationPrincipal 사용하여 userId 추출
-    @Operation(summary = "fcm 토큰 저장 API", description = "fcm 토큰을 프론트 측에서 받아온다.")
+    @Operation(summary = "FCM 토큰 저장 API", description = "사용자의 FCM 토큰을 저장한다.")
     @ApiResponse(useReturnTypeSchema = true)
-    @PostMapping("v1/fcm/token")
-    ResponseEntity<ResponseBody<FcmTokenCommonRes>> saveToken(@RequestBody FcmTokenSaveReq request);
+    @PostMapping("/v1/fcm/token")
+    ResponseEntity<ResponseBody<FcmTokenCommonRes>> saveToken(
+            @RequestBody FcmTokenSaveReq request,
+            @RequestParam Long userId);
 }
