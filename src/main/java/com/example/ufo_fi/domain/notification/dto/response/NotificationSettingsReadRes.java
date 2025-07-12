e67913866ac30ac708baf43c8a164a4ff89e8ec1
minkyeong <stl9955@naver.com>
2025-07-13 01:40:30 +0900

    feat: [UF-117] Swagger 설명 추가

---
+++ src/main/java/com/example/ufo_fi/domain/notification/dto/response/NotificationSettingsReadRes.java
@@ -0,0 +1,44 @@
+package com.example.ufo_fi.domain.notification.dto.response;
+
+import com.example.ufo_fi.domain.notification.entity.NotificationSettings;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class NotificationSettingsReadRes {
+
+    @Schema(description = "혜택 알림 수신 여부", example = "true")
+    private boolean benefit;
+
+    @Schema(description = "판매 알림 수신 여부", example = "true")
+    private boolean sell;
+
+    @Schema(description = "관심 상품 알림 수신 여부", example = "true")
+    private boolean interestedPost;
+
+    @Schema(description = "신고 누적 알림 수신 여부", example = "true")
+    private boolean reported;
+
+    @Schema(description = "팔로우한 유저의 게시글 업로드 알림 수신 여부", example = "true")
+    private boolean followerPost;
+
+    @Schema(description = "거래 전체 알림", example = "true")
+    private boolean tradeAll;
+
+    public static NotificationSettingsReadRes from(NotificationSettings settings, boolean isTradeAll) {
+        return NotificationSettingsReadRes.builder()
+                .benefit(settings.getIsEventAgreed())
+                .sell(settings.getIsSellAgreed())
+                .interestedPost(settings.getIsInterestedPostAgreed())
+                .reported(settings.getIsReportedAgreed())
+                .followerPost(settings.getIsFollowerPostAgreed())
+                .tradeAll(isTradeAll)
+                .build();
+    }
+}
