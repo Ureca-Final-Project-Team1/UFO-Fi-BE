2c026eee1a9189c16eca48c50a112e82bce1af64
minkyeong <stl9955@naver.com>
2025-07-12 23:24:16 +0900

    fix: [UF-117] 관심 상품 조건 Entity 이름 및 테이블명 수정

--- src/main/java/com/example/ufo_fi/domain/notification/entity/InterestedPost.java
+++ src/main/java/com/example/ufo_fi/domain/notification/entity/InterestedPost.java
@@ -2,29 +2,19 @@ package com.example.ufo_fi.domain.notification.entity;
 
 import com.example.ufo_fi.domain.plan.entity.Carrier;
 import com.example.ufo_fi.domain.user.entity.User;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name = "product_preferences")
+@Table(name = "interested_post")
 @Getter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class ProductPreference {
+public class InterestedPost {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
