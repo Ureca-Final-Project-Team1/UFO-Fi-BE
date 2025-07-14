e7e04cec1fbf80c2b4da49d0db6b45322e47d9b0
song hyeongyu <lit3207@ajou.ac.kr>
2025-07-14 14:05:09 +0900

    feat:UF-156 일괄 구매 dto 추가

---
+++ src/main/java/com/example/ufo_fi/domain/tradepost/dto/response/TradePostBulkPurchaseRes.java
@@ -0,0 +1,35 @@
+package com.example.ufo_fi.domain.tradepost.dto.response;
+
+import com.example.ufo_fi.domain.tradepost.entity.TradePost;
+import java.util.List;
+import java.util.stream.Collectors;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class TradePostBulkPurchaseRes {
+
+    private final int totalGb;
+    private final int totalPrice;
+    private final List<TradePostSearchDetailRes> posts;
+
+    public static TradePostBulkPurchaseRes of(List<TradePost> finalPosts) {
+
+        List<TradePostSearchDetailRes> postDetails = finalPosts.stream()
+            .map(TradePostSearchDetailRes::from)
+            .collect(Collectors.toList());
+
+        int totalGb = finalPosts.stream().mapToInt(TradePost::getSellMobileDataCapacityGb).sum();
+        int totalPrice = finalPosts.stream().mapToInt(TradePost::getTotalPrice).sum();
+
+        return TradePostBulkPurchaseRes.builder()
+            .totalGb(totalGb)
+            .totalPrice(totalPrice)
+            .posts(postDetails)
+            .build();
+    }
+
+}
