5e80860ff6d345f7c1a27ea524c02f08643365e4
minkyeong <stl9955@naver.com>
2025-07-15 17:33:41 +0900

    feat: [UF-165] 관심 상품 등록 Repository

--- src/main/java/com/example/ufo_fi/domain/notification/repository/InterestedPostRepository.java
+++ src/main/java/com/example/ufo_fi/domain/notification/repository/InterestedPostRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface InterestedPostRepository extends JpaRepository<InterestedPost, Long> {
+public interface InterestedPostRepository extends JpaRepository<InterestedPost, Long>, CustomInterestedPostRepository {
     Optional<InterestedPost> findByUserId(Long userId);
 }
