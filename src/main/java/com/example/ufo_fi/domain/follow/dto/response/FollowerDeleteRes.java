b5ddaaeeac57373b83766cd4600db4a67d426585
song hyeongyu <lit3207@ajou.ac.kr>
2025-07-15 23:36:54 +0900

    feat: UF-168 follow create, delete dto 추가

---
+++ src/main/java/com/example/ufo_fi/domain/follow/dto/response/FollowerDeleteRes.java
@@ -0,0 +1,11 @@
+package com.example.ufo_fi.domain.follow.dto.response;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class FollowerDeleteRes {
+
+    private final Long id;
+}
---
+++ src/main/java/com/example/ufo_fi/domain/follow/dto/response/FollowingCreateRes.java
@@ -0,0 +1,11 @@
+package com.example.ufo_fi.domain.follow.dto.response;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class FollowingCreateRes {
+
+    private final Long id;
+}
