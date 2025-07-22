3c5b028649e2f5f7d7398850c75a5186069c2dca
minkyeong <stl9955@naver.com>
2025-07-22 09:30:38 +0900

    feat: [UF-215] DTO 구현

---
+++ src/main/java/com/example/ufo_fi/domain/payment/dto/request/ConfirmReq.java
@@ -0,0 +1,12 @@
+package com.example.ufo_fi.domain.payment.dto.request;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class ConfirmReq {
+    private String paymentKey;
+    private String orderId;
+    private int amount;
+}
---
+++ src/main/java/com/example/ufo_fi/domain/payment/dto/request/PaymentReq.java
@@ -0,0 +1,12 @@
+package com.example.ufo_fi.domain.payment.dto.request;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class PaymentReq {
+    private String orderId;
+    private String packageName;
+    private Integer amount;
+}
