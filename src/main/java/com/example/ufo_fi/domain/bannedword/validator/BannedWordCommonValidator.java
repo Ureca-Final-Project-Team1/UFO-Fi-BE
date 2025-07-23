a5f4ae35d554c4c161c494c90e851eb773eaff6b
song hyeongyu <lit3207@ajou.ac.kr>
2025-07-23 15:39:36 +0900

    chore: UF-220 금칙어 validator 추가

---
+++ src/main/java/com/example/ufo_fi/domain/bannedword/validator/BannedWordCommonValidator.java
@@ -0,0 +1,34 @@
+package com.example.ufo_fi.domain.bannedword.validator;
+
+import com.example.ufo_fi.domain.bannedword.entity.BannedWord;
+import com.example.ufo_fi.domain.bannedword.exception.BannedWordErrorCode;
+import com.example.ufo_fi.domain.bannedword.repository.BannedWordRepository;
+import com.example.ufo_fi.global.exception.GlobalException;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class BannedWordCommonValidator {
+
+    private final BannedWordRepository bannedWordRepository;
+
+    public void validateNotDuplicated(boolean exists, BannedWordErrorCode errorCode) {
+        if (exists) {
+            throw new GlobalException(errorCode);
+        }
+    }
+
+    public void validateAllIdsExist(List<Long> ids, List<?> entities,
+        BannedWordErrorCode errorCode) {
+        if (entities.size() != ids.size()) {
+            throw new GlobalException(errorCode);
+        }
+    }
+
+    public BannedWord getBannedWord(Long id) {
+        return bannedWordRepository.findById(id)
+            .orElseThrow(() -> new GlobalException(BannedWordErrorCode.BANNED_WORD_NOT_FOUND));
+    }
+}
