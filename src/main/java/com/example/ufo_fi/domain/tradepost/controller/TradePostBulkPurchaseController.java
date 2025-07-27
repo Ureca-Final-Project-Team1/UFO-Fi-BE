f192f35cd0362c5dfd522d7d7c5f9b88b55548de
song hyeongyu <lit3207@ajou.ac.kr>
2025-07-27 20:50:19 +0900

    feat: UF-287 일괄 구매 controller 추가

--- src/main/java/com/example/ufo_fi/domain/tradepost/controller/TradePostBulkPurchaseController.java
+++ src/main/java/com/example/ufo_fi/domain/tradepost/controller/TradePostBulkPurchaseController.java
@@ -2,6 +2,8 @@ package com.example.ufo_fi.domain.tradepost.controller;
 
 import com.example.ufo_fi.domain.tradepost.controller.api.TradePostBulkPurchaseApiSpec;
 import com.example.ufo_fi.domain.tradepost.dto.request.TradePostBulkPurchaseReq;
+import com.example.ufo_fi.domain.tradepost.dto.request.TradePostConfirmBulkReq;
+import com.example.ufo_fi.domain.tradepost.dto.response.TradePostBulkPurchaseConfirmRes;
 import com.example.ufo_fi.domain.tradepost.dto.response.TradePostBulkPurchaseRes;
 import com.example.ufo_fi.domain.tradepost.service.TradePostService;
 import com.example.ufo_fi.global.response.ResponseBody;
@@ -13,15 +15,27 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 public class TradePostBulkPurchaseController implements TradePostBulkPurchaseApiSpec {
+
     private final TradePostService tradePostService;
 
     @Override
+    public ResponseEntity<ResponseBody<TradePostBulkPurchaseConfirmRes>> buyBulkPurchase(
+        TradePostConfirmBulkReq request,
+        DefaultUserPrincipal defaultUserPrincipal) {
+
+        return ResponseEntity.ok(
+            ResponseBody.success(
+                tradePostService.bulkPurchase(request, defaultUserPrincipal.getId())));
+    }
+
+    @Override
     public ResponseEntity<ResponseBody<TradePostBulkPurchaseRes>> readBulkPurchase(
-            TradePostBulkPurchaseReq request,
-            DefaultUserPrincipal defaultUserPrincipal
+        TradePostBulkPurchaseReq request,
+        DefaultUserPrincipal defaultUserPrincipal
     ) {
         return ResponseEntity.ok(
-                ResponseBody.success(
-                        tradePostService.readBulkPurchase(request, defaultUserPrincipal.getId())));
+            ResponseBody.success(
+                tradePostService.readBulkPurchase(request, defaultUserPrincipal.getId())));
     }
+
 }
