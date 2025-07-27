3103d4fc2a7fe57f3f4bcd3fa62cd49ed1a1dbf8
minkyeong <stl9955@naver.com>
2025-07-28 05:57:41 +0900

    fix: [UF-292] AuditingEntityListener 추가

--- src/main/java/com/example/ufo_fi/domain/notification/entity/NotificationHistory.java
+++ src/main/java/com/example/ufo_fi/domain/notification/entity/NotificationHistory.java
@@ -7,6 +7,7 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
@@ -16,6 +17,7 @@ import java.time.LocalDateTime;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
 public class NotificationHistory {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
