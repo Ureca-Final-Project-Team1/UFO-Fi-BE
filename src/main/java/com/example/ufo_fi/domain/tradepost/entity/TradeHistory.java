241c0746eb2f8248784a10a7023b882acfa3b3d0
song hyeongyu <lit3207@ajou.ac.kr>
2025-07-27 22:55:19 +0900

    feat: UF-290 TradeHistory 편의 메서드 추가

--- src/main/java/com/example/ufo_fi/domain/tradepost/entity/TradeHistory.java
+++ src/main/java/com/example/ufo_fi/domain/tradepost/entity/TradeHistory.java
@@ -1,15 +1,25 @@
 package com.example.ufo_fi.domain.tradepost.entity;
 
 import com.example.ufo_fi.domain.user.entity.User;
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-
-import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "trade_histories")
@@ -33,11 +43,28 @@ public class TradeHistory {
     @Column(name = "created_at")
     private LocalDateTime createdAt;
 
-    @OneToOne(fetch = FetchType.LAZY)
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "trade_post_id")
     private TradePost tradePost;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
     private User user;
+
+
+    public static TradeHistory toPurchase(TradePost tradePost, User buyer) {
+        return TradeHistory.builder()
+            .tradeType(TradeType.PURCHASE)
+            .tradePost(tradePost)
+            .user(buyer)
+            .build();
+    }
+
+    public static TradeHistory toSale(TradePost tradePost, User seller) {
+        return TradeHistory.builder()
+            .tradeType(TradeType.SALE)
+            .tradePost(tradePost)
+            .user(seller)
+            .build();
+    }
 }
