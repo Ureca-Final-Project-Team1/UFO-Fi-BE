b955971f49a5e75a117899421a0247d8a0b49c39
minkyeong <stl9955@naver.com>
2025-07-28 06:44:28 +0900

    fix: [UF-298] database 생성 및 쿠키 설정 변경

--- src/main/java/com/example/ufo_fi/global/security/oauth/CookieUtil.java
+++ src/main/java/com/example/ufo_fi/global/security/oauth/CookieUtil.java
@@ -10,15 +10,16 @@ import org.springframework.web.util.WebUtils;
 public class CookieUtil {
 
     //추후 보안 공부 해볼것.
-    public void setResponseBasicCookie(String key, String value, int expiredMs, HttpServletResponse response){
+    // TODO: 배포->로컬, 로컬->배포 시 쿠키 설정 바꾸기
+    public void setResponseBasicCookie(String key, String value, int expiredMs, HttpServletResponse response) {
         String cookieValue = String.format(
-        "%s=%s; Path=/; Max-Age=%d; HttpOnly; ",
-        key, value, expiredMs);
+                "%s=%s; Path=/; Max-Age=%d; HttpOnly; SameSite=None; Secure",
+                key, value, expiredMs);
 
         response.addHeader("Set-Cookie", cookieValue);
     }
 
-    public void deleteCookie(String key, String value, HttpServletResponse response){
+    public void deleteCookie(String key, String value, HttpServletResponse response) {
         Cookie cookie = new Cookie(key, value);
         cookie.setPath("/");
         cookie.setHttpOnly(true);
@@ -26,7 +27,7 @@ public class CookieUtil {
         response.addCookie(cookie);
     }
 
-    public Cookie getCookie(String key, HttpServletRequest request){
+    public Cookie getCookie(String key, HttpServletRequest request) {
         return WebUtils.getCookie(request, key);
     }
 }
