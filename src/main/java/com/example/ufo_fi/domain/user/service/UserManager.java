fe752faa4b1a6cc08d97e3e8a7d00b713d6af9af
song hyeongyu <lit3207@ajou.ac.kr>
2025-07-29 19:14:35 +0900

    feat: UF-319 UserManger 생성

---
+++ src/main/java/com/example/ufo_fi/domain/user/service/UserManager.java
@@ -0,0 +1,23 @@
+package com.example.ufo_fi.domain.user.service;
+
+import com.example.ufo_fi.domain.tradepost.exception.TradePostErrorCode;
+import com.example.ufo_fi.domain.user.entity.User;
+import com.example.ufo_fi.domain.user.repository.UserRepository;
+import com.example.ufo_fi.global.exception.GlobalException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class UserManager {
+
+    private final UserRepository userRepository;
+
+    public User validateUserExistence(Long userId) {
+
+        return userRepository.findById(userId)
+            .orElseThrow(() -> new GlobalException(TradePostErrorCode.USER_NOT_FOUND));
+    }
+
+
+}
