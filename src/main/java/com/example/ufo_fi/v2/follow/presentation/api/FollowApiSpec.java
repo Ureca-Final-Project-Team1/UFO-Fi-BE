6176f966cd2c6bc65a5662663ff85710453fc279
song hyeongyu <lit3207@ajou.ac.kr>
2025-07-31 09:21:07 +0900

    feat: UF-335 Follow Controller 위치 변경

--- src/main/java/com/example/ufo_fi/v2/follow/presentation/api/FollowApiSpec.java
+++ src/main/java/com/example/ufo_fi/v2/follow/presentation/api/FollowApiSpec.java
@@ -1,9 +1,11 @@
-package com.example.ufo_fi.domain.follow.controller.api;
+package com.example.ufo_fi.v2.follow.presentation.api;
 
-import com.example.ufo_fi.domain.follow.dto.response.FollowerDeleteRes;
-import com.example.ufo_fi.domain.follow.dto.response.FollowingCreateRes;
 import com.example.ufo_fi.global.response.ResponseBody;
 import com.example.ufo_fi.global.security.principal.DefaultUserPrincipal;
+import com.example.ufo_fi.v2.follow.presentation.dto.response.FollowerDeleteRes;
+import com.example.ufo_fi.v2.follow.presentation.dto.response.FollowersReadRes;
+import com.example.ufo_fi.v2.follow.presentation.dto.response.FollowingCreateRes;
+import com.example.ufo_fi.v2.follow.presentation.dto.response.FollowingsReadRes;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
@@ -11,9 +13,9 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Tag(name = "Follow API", description = "팔로워 팔로잉 API")
 public interface FollowApiSpec {
@@ -23,8 +25,8 @@ public interface FollowApiSpec {
     @PostMapping("v1/follow/{followingId}")
     @SecurityRequirement(name = "BearerAuth")
     ResponseEntity<ResponseBody<FollowingCreateRes>> createFollow(
-            @PathVariable("followingId") Long followingId,
-            @AuthenticationPrincipal DefaultUserPrincipal defaultUserPrincipal
+        @PathVariable("followingId") Long followingId,
+        @AuthenticationPrincipal DefaultUserPrincipal defaultUserPrincipal
     );
 
     @Operation(summary = "팔로워 팔로잉 끊기 API", description = "다른 유저에게 팔로우를 취소한다.")
@@ -32,6 +34,20 @@ public interface FollowApiSpec {
     @DeleteMapping("v1/unfollow/{followingId}")
     ResponseEntity<ResponseBody<FollowerDeleteRes>> deleteFollow(
         @PathVariable("followingId") Long followerId,
+        @AuthenticationPrincipal DefaultUserPrincipal defaultUserPrincipal
+    );
+
+    @Operation(summary = "내 팔로워 목록 조회 API", description = "내 계정을 팔로우 하는 유저를 조회한다.")
+    @ApiResponse(useReturnTypeSchema = true)
+    @GetMapping("/v1/mypage/followers")
+    ResponseEntity<ResponseBody<FollowersReadRes>> readFollowers(
+        @AuthenticationPrincipal DefaultUserPrincipal defaultUserPrincipal
+    );
+
+    @Operation(summary = "내 팔로잉 목록 조회 API", description = "내가 팔로우 하는 유저 목록을 조회한다.")
+    @ApiResponse(useReturnTypeSchema = true)
+    @GetMapping("/v1/mypage/followings")
+    ResponseEntity<ResponseBody<FollowingsReadRes>> readFollowings(
         @AuthenticationPrincipal DefaultUserPrincipal defaultUserPrincipal
     );
 
