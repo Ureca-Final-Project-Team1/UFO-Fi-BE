a4954ebc9d896bd1774090f597988a6238e6d571
song hyeongyu <lit3207@ajou.ac.kr>
2025-07-31 11:05:56 +0900

    feat: UF-335 FollowersCreateRes 위치 변경 및 mapper 사용

--- src/main/java/com/example/ufo_fi/v2/follow/presentation/dto/response/FollowingCreateRes.java
+++ src/main/java/com/example/ufo_fi/v2/follow/presentation/dto/response/FollowingCreateRes.java
@@ -1,6 +1,6 @@
-package com.example.ufo_fi.domain.follow.dto.response;
+package com.example.ufo_fi.v2.follow.presentation.dto.response;
 
-import com.example.ufo_fi.domain.follow.entity.Follow;
+
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -15,10 +15,4 @@ public class FollowingCreateRes {
 
     @Schema(description = "팔로잉 유저(내가 팔로우함) 아이디")
     private Long id;
-
-    public static FollowingCreateRes from(final Follow follow){
-        return FollowingCreateRes.builder()
-                .id(follow.getId())
-                .build();
-    }
 }
