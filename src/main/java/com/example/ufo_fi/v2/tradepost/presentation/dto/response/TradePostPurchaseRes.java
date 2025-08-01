878555e67b076221ed9eaed08706dd6885217f80
song hyeongyu <lit3207@ajou.ac.kr>
2025-08-01 16:55:03 +0900

    feat: UF-335 TradePostPurchaseRes method 삭제

--- src/main/java/com/example/ufo_fi/v2/tradepost/presentation/dto/response/TradePostPurchaseRes.java
+++ src/main/java/com/example/ufo_fi/v2/tradepost/presentation/dto/response/TradePostPurchaseRes.java
@@ -1,6 +1,5 @@
 package com.example.ufo_fi.v2.tradepost.presentation.dto.response;
 
-import com.example.ufo_fi.v2.user.domain.User;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -16,9 +15,5 @@ public class TradePostPurchaseRes {
     @Schema(description = "남은 ZET 총량")
     private Integer zetAsset;
 
-    public static TradePostPurchaseRes from(User buyer) {
-        return TradePostPurchaseRes.builder()
-            .zetAsset(buyer.getZetAsset())
-            .build();
-    }
+
 }
