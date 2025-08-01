e1f85a55e75d93e8168dc4b8454ca4afc1269bde
song hyeongyu <lit3207@ajou.ac.kr>
2025-08-01 16:53:36 +0900

    feat: UF-335 TradePostBulkPurchaseController 위치 변경

--- src/main/java/com/example/ufo_fi/v2/tradepost/presentation/TradePostBulkPurchaseController.java
+++ src/main/java/com/example/ufo_fi/v2/tradepost/presentation/TradePostBulkPurchaseController.java
@@ -1,13 +1,11 @@
 package com.example.ufo_fi.v2.tradepost.presentation;
 
+import com.example.ufo_fi.global.response.ResponseBody;
+import com.example.ufo_fi.global.security.principal.DefaultUserPrincipal;
 import com.example.ufo_fi.v2.tradepost.application.TradePostService;
 import com.example.ufo_fi.v2.tradepost.presentation.api.TradePostBulkPurchaseApiSpec;
 import com.example.ufo_fi.v2.tradepost.presentation.dto.request.TradePostBulkPurchaseReq;
-import com.example.ufo_fi.v2.tradepost.presentation.dto.request.TradePostConfirmBulkReq;
-import com.example.ufo_fi.v2.tradepost.presentation.dto.response.TradePostBulkPurchaseConfirmRes;
 import com.example.ufo_fi.v2.tradepost.presentation.dto.response.TradePostBulkPurchaseRes;
-import com.example.ufo_fi.global.response.ResponseBody;
-import com.example.ufo_fi.global.security.principal.DefaultUserPrincipal;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,16 +15,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class TradePostBulkPurchaseController implements TradePostBulkPurchaseApiSpec {
 
     private final TradePostService tradePostService;
-
-    @Override
-    public ResponseEntity<ResponseBody<TradePostBulkPurchaseConfirmRes>> buyBulkPurchase(
-        TradePostConfirmBulkReq request,
-        DefaultUserPrincipal defaultUserPrincipal) {
-
-        return ResponseEntity.ok(
-            ResponseBody.success(
-                tradePostService.bulkPurchase(request, defaultUserPrincipal.getId())));
-    }
 
     @Override
     public ResponseEntity<ResponseBody<TradePostBulkPurchaseRes>> readBulkPurchase(
