fb0a9b1ce6c669f4bebdfdc59016bf4b36d73738
song hyeongyu <lit3207@ajou.ac.kr>
2025-08-01 16:48:51 +0900

    feat: UF-335 SaleHistoriesRes 위치 변경 및 빌더 mapper 이동

--- src/main/java/com/example/ufo_fi/v2/tradepost/presentation/dto/response/SaleHistoriesRes.java
+++ src/main/java/com/example/ufo_fi/v2/tradepost/presentation/dto/response/SaleHistoriesRes.java
@@ -1,14 +1,5 @@
 package com.example.ufo_fi.v2.tradepost.presentation.dto.response;
 
-import static com.example.ufo_fi.v2.tradepost.domain.TradePostStatus.EXPIRED;
-import static com.example.ufo_fi.v2.tradepost.domain.TradePostStatus.REPORTED;
-import static com.example.ufo_fi.v2.tradepost.domain.TradePostStatus.SELLING;
-import static com.example.ufo_fi.v2.tradepost.domain.TradePostStatus.SOLD_OUT;
-
-import com.example.ufo_fi.v2.tradepost.domain.TradeHistory;
-import com.example.ufo_fi.v2.tradepost.domain.TradePostStatus;
-import com.example.ufo_fi.v2.tradepost.exception.TradePostErrorCode;
-import com.example.ufo_fi.global.exception.GlobalException;
 import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -23,29 +14,5 @@ public class SaleHistoriesRes {
 
     private List<? extends SaleHistoryRes> saleHistoriesRes;
 
-    public static SaleHistoriesRes from(final List<TradeHistory> tradeHistories) {
-        return SaleHistoriesRes.builder()
-            .saleHistoriesRes(
-                tradeHistories.stream()
-                    .map(history -> {
-                        TradePostStatus tradePostStatus = history.getTradePost()
-                            .getTradePostStatus();
-                        if (SOLD_OUT.equals(tradePostStatus)) {
-                            return SaleHistorySoldOutRes.from(history);
-                        }
-                        if (EXPIRED.equals(tradePostStatus)) {
-                            return SaleHistoryExpiredRes.from(history);
-                        }
-                        if (REPORTED.equals(tradePostStatus)) {
-                            return SaleHistoryReportedRes.from(history);
-                        }
-                        if (SELLING.equals(tradePostStatus)) {
-                            return SaleHistoryRes.from(history);
-                        }
-                        throw new GlobalException(TradePostErrorCode.DTO_PARSING_ERROR);
-                    })
-                    .toList()
-            )
-            .build();
-    }
+
 }
