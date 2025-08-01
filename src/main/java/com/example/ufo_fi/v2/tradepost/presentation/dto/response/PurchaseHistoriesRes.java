f7a1bb14406891389e10220b9f3db3f5f719fed6
song hyeongyu <lit3207@ajou.ac.kr>
2025-08-01 16:47:40 +0900

    feat: UF-335 PurchaseHistoriesRes 위치 변경 및 빌더 mapper 이동

--- src/main/java/com/example/ufo_fi/v2/tradepost/presentation/dto/response/PurchaseHistoriesRes.java
+++ src/main/java/com/example/ufo_fi/v2/tradepost/presentation/dto/response/PurchaseHistoriesRes.java
@@ -1,6 +1,5 @@
 package com.example.ufo_fi.v2.tradepost.presentation.dto.response;
 
-import com.example.ufo_fi.v2.tradepost.domain.TradeHistory;
 import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -14,10 +13,4 @@ import lombok.NoArgsConstructor;
 public class PurchaseHistoriesRes {
 
     private List<PurchaseHistoryRes> purchaseHistoriesRes;
-
-    public static PurchaseHistoriesRes from(final List<TradeHistory> tradeHistories) {
-        return PurchaseHistoriesRes.builder()
-            .purchaseHistoriesRes(tradeHistories.stream().map(PurchaseHistoryRes::from).toList())
-            .build();
-    }
 }
