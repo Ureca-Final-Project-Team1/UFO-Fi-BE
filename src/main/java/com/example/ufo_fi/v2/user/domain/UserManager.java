4fc42b610898382a9957fa31624472b47081ce25
minkyeong <stl9955@naver.com>
2025-08-04 19:21:12 +0900

    fix: [UF-412] userManager 수정

--- src/main/java/com/example/ufo_fi/v2/user/domain/UserManager.java
+++ src/main/java/com/example/ufo_fi/v2/user/domain/UserManager.java
@@ -1,11 +1,11 @@
 package com.example.ufo_fi.v2.user.domain;
 
-import com.example.ufo_fi.v2.userplan.presentation.dto.request.UserInfoReq;
-import com.example.ufo_fi.v2.user.exception.UserErrorCode;
-import com.example.ufo_fi.v2.tradepost.exception.TradePostErrorCode;
-import com.example.ufo_fi.v2.user.persistence.UserRepository;
 import com.example.ufo_fi.global.exception.GlobalException;
+import com.example.ufo_fi.v2.tradepost.exception.TradePostErrorCode;
 import com.example.ufo_fi.v2.user.domain.profilephoto.ProfilePhoto;
+import com.example.ufo_fi.v2.user.exception.UserErrorCode;
+import com.example.ufo_fi.v2.user.persistence.UserRepository;
+import com.example.ufo_fi.v2.userplan.presentation.dto.request.UserInfoReq;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -20,12 +20,12 @@ public class UserManager {
     public User validateUserExistence(Long userId) {
 
         return userRepository.findById(userId)
-            .orElseThrow(() -> new GlobalException(TradePostErrorCode.USER_NOT_FOUND));
+                .orElseThrow(() -> new GlobalException(TradePostErrorCode.USER_NOT_FOUND));
     }
 
     public void validateUserRole(User user, Role role) {
 
-        if(user.getRole() != role){
+        if (user.getRole() != role) {
             throw new GlobalException(UserErrorCode.INVALID_ROLE);
         }
     }
@@ -33,11 +33,11 @@ public class UserManager {
     public User findById(Long userId) {
 
         return userRepository.findById(userId)
-            .orElseThrow(() -> new GlobalException(UserErrorCode.NOT_FOUND_USER));
+                .orElseThrow(() -> new GlobalException(UserErrorCode.NOT_FOUND_USER));
     }
 
     public void updateUserNickname(
-        User user, UserInfoReq userInfoReq, String randomNickname, ProfilePhoto randomProfilePhoto
+            User user, UserInfoReq userInfoReq, String randomNickname, ProfilePhoto randomProfilePhoto
     ) {
         user.signup(userInfoReq, randomNickname, randomProfilePhoto, true, Role.ROLE_USER);
     }
@@ -76,14 +76,18 @@ public class UserManager {
     }
 
     public void validateUserPlanZetRemain(User buyer, Integer totalZet) {
-        if(buyer.getZetAsset() < totalZet){
+        if (buyer.getZetAsset() < totalZet) {
             throw new GlobalException(UserErrorCode.LACK_ZET);
         }
     }
 
     public void validateMyselfPurchase(Long userId, Long sellerId) {
-        if(userId.equals(sellerId)){
+        if (userId.equals(sellerId)) {
             throw new GlobalException(UserErrorCode.CANT_PURCHASE_MYSELF);
         }
+    }
+
+    public void zetRecovery(User user, int zet) {
+        user.increaseZetAsset(zet);
     }
 }
