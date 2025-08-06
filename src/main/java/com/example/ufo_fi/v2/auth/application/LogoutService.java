c9f85005839384019f5c90d1ff2b59fe7ec6b832
jeongjiho <wjdwlgh2000@naver.com>
2025-08-06 22:07:18 +0900

    fix: [UF-453] 로그아웃 안되는 문제 고침 3

--- src/main/java/com/example/ufo_fi/v2/auth/application/LogoutService.java
+++ src/main/java/com/example/ufo_fi/v2/auth/application/LogoutService.java
@@ -20,8 +20,6 @@ public class LogoutService {
         // 1. 응답으로 만료시간이 다 된 jwt쿠키를 추가하여, 쿠키를 삭제한다.
         jwtUtil.deleteJwtCookie(response);
         User user = userManager.findById(userId);
-
-        // 2. Refresh Token 제거
-        refreshRepository.delete(user.getRefresh());
+        user.deleteRefresh();
     }
 }
