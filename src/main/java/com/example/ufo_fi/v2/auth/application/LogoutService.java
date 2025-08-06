0501584f33d9e91ce774df343dcfc47cff9fea18
jeongjiho <wjdwlgh2000@naver.com>
2025-08-06 22:32:26 +0900

    fix: [UF-453] 로그아웃 안되는 문제 고침 5

--- src/main/java/com/example/ufo_fi/v2/auth/application/LogoutService.java
+++ src/main/java/com/example/ufo_fi/v2/auth/application/LogoutService.java
@@ -8,14 +8,15 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class LogoutService {
     private final JwtUtil jwtUtil;
-    private final RefreshRepository refreshRepository;
     private final UserManager userManager;
 
+    @Transactional
     public void logout(HttpServletRequest request, HttpServletResponse response, Long userId){
         // 1. 응답으로 만료시간이 다 된 jwt쿠키를 추가하여, 쿠키를 삭제한다.
         jwtUtil.deleteJwtCookie(response);
