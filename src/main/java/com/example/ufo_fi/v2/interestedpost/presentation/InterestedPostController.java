5113ac5b5e6b2126205f77fd2baf4716c980b29f
minkyeong <stl9955@naver.com>
2025-08-07 03:05:22 +0900

    fix: [UF-459] url 수정

--- src/main/java/com/example/ufo_fi/v2/interestedpost/presentation/InterestedPostController.java
+++ src/main/java/com/example/ufo_fi/v2/interestedpost/presentation/InterestedPostController.java
@@ -1,10 +1,11 @@
 package com.example.ufo_fi.v2.interestedpost.presentation;
 
 import com.example.ufo_fi.global.response.ResponseBody;
+import com.example.ufo_fi.v2.auth.application.principal.DefaultUserPrincipal;
 import com.example.ufo_fi.v2.interestedpost.application.InterestedPostService;
 import com.example.ufo_fi.v2.interestedpost.presentation.api.InterestedPostApiSpec;
 import com.example.ufo_fi.v2.interestedpost.presentation.dto.request.InterestedPostUpdateReq;
-import com.example.ufo_fi.v2.auth.application.principal.DefaultUserPrincipal;
+import com.example.ufo_fi.v2.interestedpost.presentation.dto.response.InterestedPostRes;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,5 +24,12 @@ public class InterestedPostController implements InterestedPostApiSpec {
 
         interestedPostService.updateInterestedPost(defaultUserPrincipal.getId(), request);
         return ResponseEntity.ok(ResponseBody.noContent());
+    }
+
+    @Override
+    public ResponseEntity<ResponseBody<InterestedPostRes>> readInterestedPost(
+            DefaultUserPrincipal defaultUserPrincipal
+    ) {
+        return ResponseEntity.ok(ResponseBody.success(interestedPostService.readInterestedPost(defaultUserPrincipal.getId())));
     }
 }
